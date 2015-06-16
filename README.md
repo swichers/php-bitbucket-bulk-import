932868e2a2f3f40e1ca95eecf1720d91959e22c8
Steven Wichers <steven.wichers@gmail.com>
2015-06-15 17:31:32 -0700

    Initial code commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# BitBucket bulk importer
+
+Just a quick script to leverage BitBucket's repository import feature and import
+a lot of repositories in one go. They currently do not have an API endpoint for
+this functionality.
+
+# Usage
+
+Origin repositories must be world accessible, or you must update the code to
+handle specifying authentication information to the BitBucket importer.
+
+First you must create a .env file with the correct information See .env.sample
+for what is required.
+
+Second you must have a CSV file with the following column types, in order:
+
+`"Repo path","Project name","Project path","Namespace Path","Project ID","wiki_enabled","New name","description"`
+
+The column names do not matter. Alternatively you can modify the `$get_repos`
+function to suit your data format (recommended).
+
+The final step is to run `php -f migrate-repos.php` and allow it to process your
+repository list. You should get e-mails from BitBucket as the repositories
+complete.
+
+# Note! Important!
+
+The script as-is is customized for my specific setup. Some of it has been
+abstracted out into a .env file, but key areas to work with are changing
+`$default_data` to suit your needs, the `$get_repos` to change how the CSV info
+gets mapped, and `$repo_data` if you don't have a `new_name` key in your repo
+information.
---
+++ migrate-repos.php
@@ -0,0 +1,116 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'vendor/autoload.php';
+
+$env = new Dotenv\Dotenv(__DIR__);
+$env->load();
+$env->required([
+    'CSV_FILE',
+    'USERNAME',
+    'PASSWORD',
+    'OWNER_ID',
+    'ORIGIN_BASE',
+  ])->notEmpty();
+
+define('CSV_FILE', getenv('CSV_FILE'));
+define('USERNAME', getenv('USERNAME'));
+define('PASSWORD', getenv('PASSWORD'));
+define('OWNER_ID', getenv('OWNER_ID'));
+define('ORIGIN_BASE', getenv('ORIGIN_BASE'));
+define('BITBUCKET_BASE', 'https://bitbucket.org');
+define('BITBUCKET_IMPORT', 'https://bitbucket.org/repo/import');
+
+$default_data = [
+  'source_scm' => 'git',
+  'source' => 'source-git',
+  'goog_project_name' => '',
+  'goog_scm' => 'svn',
+  'sourceforge_project_name' => '',
+  'sourceforge_mount_point' => '',
+  'sourceforge_scm' => 'svn',
+  'codeplex_project_name' => '',
+  'codeplex_scm' => 'svn',
+  'url' => '',
+  'auth' => FALSE,
+  'owner' => OWNER_ID,
+  'name' => '',
+  'description' => '',
+  'is_private' => TRUE,
+  'forking' => 'no_forks',
+  'no_forks' => FALSE,
+  'no_public_forks' => TRUE,
+  'has_wiki' => FALSE,
+  'language' => 'php',
+];
+
+// Pull repository information out of a CSV file.
+$get_repos = function ($fn) {
+
+  $repos = [];
+  $fh = fopen($fn, 'r');
+
+  while ($line = fgetcsv($fh)) {
+
+    $repos[] = [
+      'path' => $line[0],
+      'name' => $line[1],
+      'wiki' => $line[5],
+      'new_name' => $line[6],
+      'description' => $line[7],
+    ];
+  }
+
+  // Remove the header from the CSV file.
+  if (!empty($repos)) {
+
+    unset($repos[0]);
+  }
+
+  fclose($fh);
+
+  return $repos;
+};
+
+$repos = $get_repos(CSV_FILE);
+if (empty($repos)) {
+
+  echo 'No repositories found.', PHP_EOL;
+  exit(1);
+}
+
+printf("Got %d repositories from file.\n", count($repos));
+
+$client = new GuzzleHttp\Client([
+    'base_url' => BITBUCKET_BASE,
+    'cookies' => true,
+    'headers' => ['Referer' => BITBUCKET_BASE],
+    'auth' => [USERNAME, PASSWORD],
+  ]);
+
+// Go through each repository and start an import job.
+foreach ($repos as $info) {
+
+  // Build up our repo specific data based on our CSV file.
+  $repo_data = [
+    'url' => sprintf('%s/%s', ORIGIN_BASE, $info['path']),
+    'name' => empty($info['new_name']) ? $info['name'] : $info['new_name'],
+    'description' => $info['description'],
+    'has_wiki' => !empty($info['wiki']),
+  ];
+  $data = array_merge($default_data, $repo_data);
+
+  printf("Starting import job for '%s' from '%s'\n", $data['name'], $info['path']);
+
+  $resp = $client->post(BITBUCKET_IMPORT, ['form_params' => $data]);
+  if ($resp->getStatusCode() != 200) {
+
+    printf("Error importing project (Response: %d)\n", $resp->getStatusCode());
+    return;
+  }
+}
+
+echo 'Done', PHP_EOL;
+exit(0);
